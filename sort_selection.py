c3ec858d0f471638de666bbc1f3501a7636aed9b
2007-pilar <pilybarrionuevo10@gmail.com>
2025-11-14 12:36:44 -0300

    Update sort_selection.py

--- sort_selection.py
+++ sort_selection.py
@@ -18,25 +18,27 @@ def step():
      global items, n,i,j, min_idx,fase                                         
     if i>=n:
        return {"done":True}  
-    if fase=="buscar":
-       if j<n: 
-          if items[j]<= items[min_idx]:
-             min_idx=j
-          j_actual=j
-          j=j+1
-          return {"a": min_idx, "b": j_actual, "swap": False, "done": False}
-       else: #   Al terminar el barrido, pasar a fase "swap".
-             fase="swap"
-             return step()
-    if fase== "swap":
-       if min_idx !=i:
-          items[i], items[min_idx]= items[min_idx], items[i] 
-          swap_unico = {"a":i, "b":min_idx, "swap":True, "done":False}
-        else: 
-            swap_unico={ "a":i, "b": min_idx, "swap": False, "done": False}
-       i=i+1
-       j=i+1
-       min_idx=i
-       fase="buscar" 
-        # Cuando i llegue al final, devolvé {"done": True}.
-    return swap_unico 
+    for i in range(1):
+        if fase=="buscar":
+            if j<n: 
+               if items[j]<= items[min_idx]:
+                   min_idx=j
+                   j_actual=j
+                   j=j+1
+                  return {"a": min_idx, "b": j_actual, "swap": False, "done": False}
+          else: #   Al terminar el barrido, pasar a fase "swap".
+               fase="swap"
+               return step()
+       if fase== "swap":
+           for i in range(1):
+                 if min_idx !=i:
+                    items[i], items[min_idx]= items[min_idx], items[i] 
+                     swap_unico = {"a":i, "b":min_idx, "swap":True, "done":False}
+                 else: 
+                      swap_unico={ "a":i, "b": min_idx, "swap": False, "done": False}
+                 i=i+1
+                 j=i+1
+                 min_idx=i
+                 fase="buscar" 
+                # Cuando i llegue al final, devolvé {"done": True}.
+                  return swap_unico 
