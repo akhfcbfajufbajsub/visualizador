d88bbb9cadbadfd7fc08ff6dfd8f536ba2f15f7a
2007-pilar <pilybarrionuevo10@gmail.com>
2025-11-14 20:05:56 -0300

    Update sort_selection.py

--- sort_selection.py
+++ sort_selection.py
@@ -16,24 +16,24 @@ def init(vals):
     
 def step():
      global items, n,i,j, min_idx,fase                                         
-    if i>=n:
-       return {"done":True} 
-        if fase=="buscar":
-            if j<n: 
-               if items[j]<= items[min_idx]:
-                   min_idx=j
-                   j_actual=j
-                   j=j+1
+     if i>=n:
+        return {"done":True} 
+     if fase=="buscar":
+         if j<n: 
+             if items[j]<= items[min_idx]:
+                  min_idx=j
+                  j_actual=j
+                  j=j+1
                   return {"a": min_idx, "b": j_actual, "swap": False, "done": False}
-         else: #   Al terminar el barrido, pasar a fase "swap".
-            fase="swap"
-            return step()
-         if fase== "swap":
-             if min_idx !=i:
-                items[i], items[min_idx]= items[min_idx], items[i] 
-                swap_unico = {"a":i, "b":min_idx, "swap":True, "done":False}
-             else: 
-                swap_unico={ "a":i, "b": min_idx, "swap": False, "done": False}
+          else: 
+              fase="swap"
+              return step()
+      if fase== "swap":
+         if min_idx !=i:
+              items[i], items[min_idx]= items[min_idx], items[i] 
+              swap_unico = {"a":i, "b":min_idx, "swap":True, "done":False}
+         else: 
+             swap_unico={ "a":i, "b": min_idx, "swap": False, "done": False}
          i=i+1
          j=i+1
          min_idx=i
