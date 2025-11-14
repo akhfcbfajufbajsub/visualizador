b45907049b1b805417d6fbefddde87c9f7a93c7e
2007-pilar <pilybarrionuevo10@gmail.com>
2025-11-14 12:50:38 -0300

    Update sort_selection.py

--- sort_selection.py
+++ sort_selection.py
@@ -1,9 +1,9 @@
 items = []
 n = 0
-i = 0          # cabeza de la parte no ordenada
-j = 0          # cursor que recorre y busca el mínimo
-min_idx = 0    # índice del mínimo de la pasada actual
-fase = "buscar"  # "buscar" | "swap"
+i = 0          
+j = 0          
+min_idx = 0    
+fase = "buscar"
 
 def init(vals):
     global items, n, i, j, min_idx, fase
@@ -17,8 +17,7 @@ def init(vals):
 def step():
      global items, n,i,j, min_idx,fase                                         
     if i>=n:
-       return {"done":True}  
-    for i in range(1):
+       return {"done":True} 
         if fase=="buscar":
             if j<n: 
                if items[j]<= items[min_idx]:
@@ -26,19 +25,17 @@ def step():
                    j_actual=j
                    j=j+1
                   return {"a": min_idx, "b": j_actual, "swap": False, "done": False}
-          else: #   Al terminar el barrido, pasar a fase "swap".
-               fase="swap"
-               return step()
-       if fase== "swap":
-           for i in range(1):
-                 if min_idx !=i:
-                    items[i], items[min_idx]= items[min_idx], items[i] 
-                     swap_unico = {"a":i, "b":min_idx, "swap":True, "done":False}
-                 else: 
-                      swap_unico={ "a":i, "b": min_idx, "swap": False, "done": False}
-                 i=i+1
-                 j=i+1
-                 min_idx=i
-                 fase="buscar" 
-                # Cuando i llegue al final, devolvé {"done": True}.
-                  return swap_unico 
+         else: #   Al terminar el barrido, pasar a fase "swap".
+            fase="swap"
+            return step()
+         if fase== "swap":
+             if min_idx !=i:
+                items[i], items[min_idx]= items[min_idx], items[i] 
+                swap_unico = {"a":i, "b":min_idx, "swap":True, "done":False}
+             else: 
+                swap_unico={ "a":i, "b": min_idx, "swap": False, "done": False}
+         i=i+1
+         j=i+1
+         min_idx=i
+         fase="buscar" 
+         return swap_unico 
